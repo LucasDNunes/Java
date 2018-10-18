337ec46a785fc444ae881985fb03f20d01880e9f
Lucas <lucasnunes-tb@hotmail.com>
2018-10-17 23:57:25 -0300

    comentarios para me ajudar na prova

--- Unisul/Topicos_Avancados/Files/src/view/CadastroTransporteController.java
+++ Unisul/Topicos_Avancados/Files/src/view/CadastroTransporteController.java
@@ -8,7 +8,6 @@ import javafx.scene.control.RadioButton;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
-import model.aluno.Aluno;
 import model.transporte.Transporte;
 import model.transporte.TransporteServiceImpl;
 
@@ -31,8 +30,7 @@ public class CadastroTransporteController {
 	
 	private List<Transporte> transportes =  new ArrayList<>();
 	
-	private TransporteServiceImpl transporteServiceImpl = new TransporteServiceImpl(); 
-	
+	private TransporteServiceImpl transporteServiceImpl = new TransporteServiceImpl();
 	@FXML
 	public void initialize() {
 		transporteServiceImpl.inicializaTableView(colTipo, colDistancia, colCusto);
@@ -49,4 +47,26 @@ public class CadastroTransporteController {
 	public void unirArquivos() {
 		transporteServiceImpl.unirAlunoETransporte();
 	}
+	
+	/*  PARA ME AJUDAR NA ATIVIDADE PARA PROVA
+	 	System.out.print(String.format("%-10.10s%s", "teste1234590", "hue"));
+		System.out.println(".");
+		System.out.print(String.format(">%-10s<", "testeadest182".substring(0, 10)));
+		System.out.println(".");
+
+		
+		String numero = String.format("%010d", 10);
+		System.out.println(numero);
+		numero = Integer.valueOf(numero).toString();
+		System.out.println(numero);
+		
+		String doub = String.format("%.2f", 106565.00);
+		System.out.println(doub);
+		DecimalFormat decimalFormat = new DecimalFormat("0.00");
+		System.out.println(decimalFormat.format(48)); 
+	 
+	 */
+	
+	
+	
 }
