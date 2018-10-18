bf6d12865d4810e9e30c5c4c9745c22a1a08ef38
Lucas <lucasnunes-tb@hotmail.com>
2018-10-18 00:13:52 -0300

    Revert "aula dia 18-10-2018"
    This reverts commit 3b51369f9fb4fe39e2ed2c48a3ac1446f6c8c1f6.

--- Unisul/Topicos_Avancados/Files/src/view/CadastroAlunoController.java
+++ Unisul/Topicos_Avancados/Files/src/view/CadastroAlunoController.java
@@ -8,6 +8,7 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import model.aluno.Aluno;
+import model.aluno.AlunoService;
 import model.aluno.AlunoServiceImpl;
 
 public class CadastroAlunoController {
@@ -20,20 +21,20 @@ public class CadastroAlunoController {
 	@FXML TableColumn<Aluno, Number> colSemestre;
 	@FXML TableColumn<Aluno, String> colCurso;
 	
-	AlunoServiceImpl alunoServiceImpl =  new AlunoServiceImpl();
+	private AlunoService alunoService =  new AlunoServiceImpl();
 	
 	private List<Aluno> alunos = new ArrayList<>();
 	
 	@FXML
 	public void initialize() {
-		alunoServiceImpl.inicializaTableView(colNome,colSemestre,colCurso);
-		alunoServiceImpl.lerArquivo(alunos, tableView);
+		alunoService.inicializaTableView(colNome,colSemestre,colCurso);
+		alunoService.lerArquivo(alunos, tableView);
 	}
 	
 	@FXML
 	public void cadastrar() {
-		alunoServiceImpl.gravar(txtNome,txtSemestre,txtCurso);
-		alunoServiceImpl.lerArquivo(alunos, tableView);
+		alunoService.gravar(txtNome,txtSemestre,txtCurso);
+		alunoService.lerArquivo(alunos, tableView);
 	}
 	
 }
