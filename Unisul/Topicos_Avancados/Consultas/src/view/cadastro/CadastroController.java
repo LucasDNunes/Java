f5de04c2e3d5a22d31e0708bdd62add9ce853df9
Lucas <lucasnunes-tb@hotmail.com>
2018-11-13 00:56:38 -0200

    trabalho final - inicio

---
+++ Unisul/Topicos_Avancados/Consultas/src/view/cadastro/CadastroController.java
@@ -0,0 +1,5 @@
+package view.cadastro;
+
+public class CadastroController {
+
+}
---
+++ Unisul/Topicos_Avancados/Consultas/src/view/main/PricnipalController.java
@@ -0,0 +1,47 @@
+package view.main;
+
+import java.util.Objects;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+
+public class PricnipalController {
+	@FXML
+	TabPane tabPane;
+	
+	@FXML
+	public void abreTelaCadastro() {
+		abreTab("Cadastro", "../cadastro/cadastro.fxml");
+	}
+	
+	private void abreTab(String titulo, String path) {
+		try {
+			Tab tab = tabAberta(titulo);
+			if (tab == null) {
+				tab = new Tab(titulo);
+				tab.setClosable(true);
+				tabPane.getTabs().add(tab);
+				tab.setContent((Node) FXMLLoader.load(getClass().getResource(path)));
+			}
+			selecionaTab(tab);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void selecionaTab(Tab tab) {
+		tabPane.getSelectionModel().select(tab);
+	}
+	
+	private Tab tabAberta(String titulo) {
+		for (Tab tab : tabPane.getTabs()) {
+			if (Objects.nonNull(tab.getText()) && tab.getText().equals(titulo)) {
+				return tab;
+			}
+		}
+		return null;
+	}
+}
