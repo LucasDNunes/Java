c1c286a59c794114f1a2682a2d18fa35f2fb6c9e
Lucas <lucasnunes-tb@hotmail.com>
2019-01-11 23:58:14 -0200

    create and find

--- Alura/Java-JPA/financas/src/test/java/modelos/ContaTest.java
+++ Alura/Java-JPA/financas/src/test/java/modelos/ContaTest.java
@@ -1,16 +1,15 @@
 package modelos;
 
+import Util.JPAUtil;
+import org.junit.Assert;
 import org.junit.Test;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 
 public class ContaTest {
 
     @Test
-    public void testCreate(){
-
+    public void testCreate() {
         Conta conta = Conta.builder()
                 .titular("Leonardo")
                 .agencia("123")
@@ -18,14 +17,88 @@ public class ContaTest {
                 .numero("456")
                 .build();
 
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("financas");
-        EntityManager em = emf.createEntityManager();
-        em.getTransaction().begin();
+        EntityManager manager = new JPAUtil().getEntityManager();
+        manager.getTransaction().begin();
 
-        em.persist(conta);
+        manager.persist(conta);
+        Assert.assertNotNull(conta.getId());
 
-        em.getTransaction().commit();
-        em.close();
-        emf.close();
+        manager.getTransaction().commit();
+
+        manager.remove(conta);
+        manager.close();
+    }
+
+    @Test
+    public void populaBanco() {
+
+        EntityManager manager = new JPAUtil().getEntityManager();
+        manager.getTransaction().begin();
+//        manager.createQuery("delete from Conta").executeUpdate();
+
+        Conta conta = Conta.builder()
+                .banco("001 - BANCO DO BRASIL")
+                .numero("16987-8")
+                .agencia("6543")
+                .titular("Maria dos Santos")
+                .build();
+
+        manager.persist(conta);
+
+        conta = Conta.builder()
+                .banco("237 - BANCO BRADESCO")
+                .numero("86759-1")
+                .agencia("1745")
+                .titular("Paulo Roberto Souza")
+                .build();
+
+        manager.persist(conta);
+
+        conta = Conta.builder()
+                .banco("341 - BANCO ITAU UNIBANCO")
+                .numero("46346-3")
+                .agencia("4606")
+                .titular("Antonio Duraes")
+                .build();
+        manager.persist(conta);
+
+        conta = Conta.builder()
+                .banco("033 - BANCO SANTANDER")
+                .numero("12345-6")
+                .agencia("9876")
+                .titular("Leandra Marques")
+                .build();
+
+        manager.persist(conta);
+
+        conta = Conta.builder()
+                .banco("104 - CAIXA ECONOMICA FEDERAL")
+                .numero("98654-3")
+                .agencia("1234")
+                .titular("Alexandre Duarte")
+                .build();
+
+        manager.persist(conta);
+
+        manager.getTransaction().commit();
+    }
+
+    @Test
+    public void testUpdateConta(){
+        EntityManager manager = new JPAUtil().getEntityManager();
+        manager.getTransaction().begin();
+
+        Conta conta = manager.find(Conta.class,15L);
+
+        Assert.assertNotNull(conta);
+
+        conta.setTitular("João da Silva");
+
+        manager.getTransaction().commit();
+
+        Conta contaNova = manager.find(Conta.class, 15L);
+        Assert.assertNotNull(contaNova);
+        Assert.assertEquals(conta.getTitular(), contaNova.getTitular());
+
     }
 }
